9a1593aebf26ea4f65325722f43d8bd30a38b59a
Chamika Weerasekara <>
2025-01-14 14:24:56 +0530

    cart page bug fixes added.

--- lib/features/cart/ui/cart_page.dart
+++ lib/features/cart/ui/cart_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_sample_shoping_app/features/home/bloc/home_bloc.dart';
 
 import '../../home/models/home_product_data_model.dart';
-import '../../home/ui/product_tile_widget.dart';
 import '../bloc/cart_bloc.dart';
 import 'cart_tile_widget.dart';
 
@@ -15,7 +13,6 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-
   final CartBloc cartBloc = CartBloc();
 
   @override
@@ -26,84 +23,62 @@ class _CartPageState extends State<CartPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<CartBloc, CartState>(
-      bloc: cartBloc,
-      listenWhen: (previous,current){
-        return current is CartActionState;
-      },
-      buildWhen: (previous,current){
-        return current is !CartActionState;
-      },
-      listener: (context, state) {
-        if(state is CartItemRemovedState){
-          // need to show a snack bar
-        }
-      },
-      builder: (context, state) {
-        if(state is CartLoadingState){
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.teal,
-              title: const Text(
-                "Cart",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            body: const Center(child: CircularProgressIndicator()),
-          );
-        }else if(state is CartSuccessState){
-           final List<ProductDataModel> cartList = state.cartItems;
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: const Text(
+            "Cart",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: BlocConsumer<CartBloc, CartState>(
+            listener: (context, state) {
+              // TODO: implement listener
+            },
+            bloc: cartBloc,
+            listenWhen: (previous, current) {
+              return current is CartActionState;
+            },
+            buildWhen: (previous, current) {
+              return current is! CartActionState;
+            },
+            builder: (context, state) {
+              if (state is CartLoadingState) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (state is CartSuccessState) {
+                final List<ProductDataModel> cartList = state.cartItems;
 
-           if(cartList.isNotEmpty) {
-             return Scaffold(
-               appBar: AppBar(
-                 backgroundColor: Colors.teal,
-                 title: const Text(
-                   "Cart",
-                   style: TextStyle(color: Colors.white),
-                 ),
-               ),
-               body: Column(
-                   children: [
-                     const SizedBox(height: 20),
-                     Expanded(
-                       child: ListView.builder(
-                           itemCount: cartList.length,
-                           itemBuilder: (context, index) {
-                             return CartTileWidget(
-                                 cartBloc: cartBloc,
-                                 productDataModel: cartList[index]);
-                           }),
-                     ),
-                   ]),
-             );
-           }else{
-             return const Center(
-                 child: Text("Product List is Empty",style: TextStyle(
-                     fontSize: 24, fontWeight: FontWeight.bold),)
-             );
-           }
-        }else if(state is CartErrorState){
-          final errorMessage = state.errorMessage;
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.teal,
-              title: const Text(
-                "Grocery Bloc App",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            body: Center(
-              child: Text(
-                errorMessage,
-                style: const TextStyle(fontSize: 20),
-              ),
-            ),
-          );
-        }else{
-          return Container();
-        }
-      },
-    );
+                if (cartList.isNotEmpty) {
+                  return Column(children: [
+                    const SizedBox(height: 20),
+                    Expanded(
+                      child: ListView.builder(
+                          itemCount: cartList.length,
+                          itemBuilder: (context, index) {
+                            return CartTileWidget(
+                                cartBloc: cartBloc,
+                                productDataModel: cartList[index]);
+                          }),
+                    ),
+                  ]);
+                } else {
+                  return const Center(
+                      child: Text(
+                    "Product List is Empty",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ));
+                }
+              } else if (state is CartErrorState) {
+                final errorMessage = state.errorMessage;
+                return Center(
+                  child: Text(
+                    errorMessage,
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                );
+              } else {
+                return Container();
+              }
+            }));
   }
 }
